85725d7e0168e238408441f50aca1b9b5ea4b21e
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-05-20 22:28:30 -0400

    added visitors index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  root to: 'visitors#index'
   devise_for :users
-  # root to: 'home#index'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
